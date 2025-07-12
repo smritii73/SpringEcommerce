5c94b61c35335cd529f892da2c4126ffa3dfa77b
Smriti Dube <smritiiii04@gmail.com>
2025-07-12 18:21:21 +0530

    Added all the Low Level Design Code

--- src/main/java/com/example/ecommercespring/dto/FakeStoreProductResponseDTO.java
+++ src/main/java/com/example/ecommercespring/dto/FakeStoreProductResponseDTO.java
@@ -12,4 +12,5 @@ public class FakeStoreProductResponseDTO{
 	private ProductDTO product;
 	private String message;
 	private String status;
+
 }
--- src/main/java/com/example/ecommercespring/services/FakeStoreProductService.java
+++ src/main/java/com/example/ecommercespring/services/FakeStoreProductService.java
@@ -1,13 +1,21 @@
 package com.example.ecommercespring.services;
 
 import com.example.ecommercespring.dto.ProductDTO;
+import com.example.ecommercespring.gateway.FakeStoreProductGateway;
+import org.springframework.stereotype.Service;
 
 import java.io.IOException;
-
+@Service
 public class FakeStoreProductService implements IProductService{
+    private final FakeStoreProductGateway fakeStoreProductGateway;
+
+    public FakeStoreProductService(FakeStoreProductGateway fakeStoreProductGateway) {
+        this.fakeStoreProductGateway = fakeStoreProductGateway;
+    }
 
     @Override
     public ProductDTO getProductById(Long id) throws IOException {
-        return null;
+        ProductDTO response = this.fakeStoreProductGateway.getProductById(id);
+        return response;
     }
 }
